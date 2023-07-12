8ce52eecb59e7104f87e387e4764da7e91ff8dc2
hamza <mahamza415@gmail.com>
2023-07-12 12:24:57 +0500

    login add

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const registerUser = require("../controllers/userControllers/registerUser")
-
+const loginUser = require("../controllers/userControllers/loginUser");
 // @desc        create user 
 // route        http://localhost:6090/api/user/register
 // method       post
 
 router.route("/register").post(registerUser);
 
+// @desc        create user 
+// route        http://localhost:6090/api/user/login
+// method       post
+router.route("/login").post(loginUser);
 module.exports = router;
