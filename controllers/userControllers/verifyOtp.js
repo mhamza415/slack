d6d240e7c491cfc04d84ab795962a2b44f616b35
ObaidUr Rehman <link2ubaid98@gmail.com>
2023-07-13 15:18:08 +0500

    channel management

--- controllers/userControllers/verifyOtp.js
+++ controllers/userControllers/verifyOtp.js
@@ -5,28 +5,34 @@ const verifyOtp = async (req, res) => {
   try {
     const { email, otp } = req.body;
     if (!validator.isEmail(email)) {
-      res.status(404).send({ success: false, message: "email is not valid" });
+      return res
+        .status(400)
+        .send({ success: false, message: "email is not valid" });
     }
     if (!otp) {
-      res.status(404).send({ success: false, message: "otp cannot be null" });
+      return res
+        .status(400)
+        .send({ success: false, message: "otp cannot be null" });
     }
     const user = await User.findOne({ where: { email: email } });
-    //console.log("user", user);
+    console.log("user", user.dataValues.email);
     if (!user) {
-      res.status(404).send({ success: false, message: "user is invalid" });
+      return res
+        .status(400)
+        .send({ success: false, message: "user is invalid" });
     }
     let currentTime = new Date();
     //console.log(user.otp);
     //console.log(user.otp_expires);
     if (user && user.otp === otp && currentTime < user.otp_expires) {
-      res.status(201).json({
+      return res.status(201).json({
         id: user.id,
         email: user.email,
         role: user.role,
         token: generateToken(),
       });
     } else {
-      res
+      return res
         .status(404)
         .send({ success: false, message: "otp is expired or incorrect" });
     }
