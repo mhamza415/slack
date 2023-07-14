926be3dd467f732c2ecead7fdc8f236b4e9ce9d9
hamza <mahamza415@gmail.com>
2023-07-14 22:59:56 +0500

    dockerized the app

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -17,14 +17,14 @@ router.route("/send").post(protect, () => { });
 router.route("/get").get(protect, () => { });
 
 // @desc        register user in channel
-// route        http://localhost:6090/api/channel/registerUserInChannel
+// route        http://localhost:6090/api/message/update
 // method       post
 // you need to add the token for hitting this route
 
 router.route("/update").post(protect, () => { });
 
 // @desc        remove user from channel
-// route        http://localhost:6090/api/channel/removeuserchannel
+// route        http://localhost:6090/api/message/delete
 // method       post
 // you need to add the token for hitting this route
 
