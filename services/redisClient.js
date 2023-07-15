b793d45832b467b52fe127411a01509eecde642f
hamza <mahamza415@gmail.com>
2023-07-15 21:10:19 +0500

    added docker container redis

--- services/redisClient.js
+++ services/redisClient.js
@@ -1,14 +1,38 @@
-const redis = require("redis");
-const redisclient = redis.createClient();
 
-(async function getMessages() {
+const { createClient } = require('redis');
+
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  }
+});
+
+(async () => {
   console.log("Connecting to Redis...");
 })();
 
-redisclient.on("ready", () => {
+client.on("ready", () => {
   console.log("Connected to Redis!");
 });
 
-redisclient.on("error", (err) => {
+client.on("error", (err) => {
   console.log("Error in Redis connection:", err);
 });
+
+
+// const redis = require("redis");
+// const redisclient = redis.createClient();
+
+// (async function getMessages() {
+//   console.log("Connecting to Redis...");
+// })();
+
+// redisclient.on("ready", () => {
+//   console.log("Connected to Redis!");
+// });
+
+// redisclient.on("error", (err) => {
+//   console.log("Error in Redis connection:", err);
+// });
